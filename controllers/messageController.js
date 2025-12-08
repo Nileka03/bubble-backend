f2bca4a4f8246134dd4468aef0437bb45b4d4cf6
nileka3113 <nilekakavisandi@gmail.com>
2025-12-08 13:52:28 +0530

    add emotion analysis

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,6 +2,7 @@ import Message from "../models/message.js";
 import User from "../models/User.js";
 import cloudinary from "../lib/cloudinary.js";
 import { io, userSocketMap } from "../server.js";
+import { analyzeMood } from "./aiController.js"; 
 
 export const getUserForSidebar = async (req, res) => {
     try {
@@ -61,9 +62,6 @@ export const getMessages = async (req, res) => {
 
         res.json({ success: true, messages })
 
-
-
-
     } catch (error) {
         console.log(error.message);
         res.json({ success: false, message: error.message })
@@ -95,7 +93,6 @@ export const sendMessage = async (req, res) => {
         const receiverId = req.params.id;
         const senderId = req.user._id;
 
-
         let imageUrl;
         if (image) {
             const uploadResponse = await cloudinary.uploader.upload(image)
@@ -110,9 +107,41 @@ export const sendMessage = async (req, res) => {
 
         //emit the new message to the receiver's socket
         const receiverSocketId = userSocketMap[receiverId];
+        const senderSocketId = userSocketMap[senderId]; // Get sender socket too for mood update
+
         if (receiverSocketId) {
             io.to(receiverSocketId).emit("newMessage", newMessage)
         }
+
+        // mood analysis
+        // fetch context (Last 5 messages including the new one)
+        const recentMessages = await Message.find({
+            $or: [
+                { senderId: senderId, receiverId: receiverId },
+                { senderId: receiverId, receiverId: senderId },
+            ],
+        })
+        .sort({ createdAt: -1 })
+        .limit(5);
+
+        // Prepare data
+        const historyForAI = recentMessages.reverse().map(msg => ({
+            sender: msg.senderId.toString() === senderId.toString() ? "me" : "partner",
+            text: msg.text || (msg.image ? "[Image sent]" : ""),
+        }));
+
+        // trigger analysis
+        analyzeMood(historyForAI).then((moodData) => {
+            // Emit 'moodUpdate' to both users so the ring changes for everyone
+            if (receiverSocketId) {
+                io.to(receiverSocketId).emit("moodUpdate", moodData);
+            }
+            if (senderSocketId) {
+                io.to(senderSocketId).emit("moodUpdate", moodData);
+            }
+        }).catch(err => console.error("Mood analysis background error:", err));
+
+        
 
         res.json({ success: true, newMessage });
     }
